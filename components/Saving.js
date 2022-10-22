2eb6dfdcbc1c62832a13b8423b3c9be82b747344
Tengis <59511790+TengisCh@users.noreply.github.com>
2022-10-22 17:39:11 +0200

    prisma/migration delete, package.json change

--- components/Saving.js
+++ components/Saving.js
@@ -1,57 +1,67 @@
-import Link from 'next/link'
-import moment from 'moment';
+import Link from "next/link";
+import moment from "moment";
 import {
-  Progress, Card,
+  Progress,
+  Card,
   CardHeader,
   CardBody,
   CardFooter,
-  Typography
+  Typography,
 } from "@material-tailwind/react";
 
 export default function Saving({ saving }) {
-
   return (
     <>
-      <div className='flex flex-col mb-4 mx-20 my-10 items-center'>
+      <div className="flex flex-col mb-4 mx-20 my-10 items-center">
         <Card className="w-1/2">
           <CardBody>
-            <div className='flex justify-between'>
+            <div className="flex justify-between">
               <div>
                 <Typography variant="h5" className="mb-2">
                   <Link href={`/saving/${saving.id}`}>
-                    <a style={{color: '#253655'}} className='flex-shrink text-2xl font-bold width-auto'>
+                    <a
+                      style={{ color: "#253655" }}
+                      className="flex-shrink text-2xl font-bold width-auto"
+                    >
                       {saving.title}
                     </a>
                   </Link>
                 </Typography>
               </div>
               <div>
-                <button type="button" style={{color: '#00B9FF', backgroundColor: '#F2F5F7'}} class="focus:ring-4 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-xs px-3 py-1.5 mr-2 text-center inline-flex items-center dark:bg-blue-800 dark:hover:bg-blue-900">
+                <button
+                  type="button"
+                  style={{ color: "#00B9FF", backgroundColor: "#F2F5F7" }}
+                  className="focus:ring-4 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-xs px-3 py-1.5 mr-2 text-center inline-flex items-center dark:bg-blue-800 dark:hover:bg-blue-900"
+                >
                   Insufficient Found
                 </button>
               </div>
             </div>
+            <Typography>{saving.content}</Typography>
             <Typography>
-              {saving.content}
+              Frequency: {saving.period} - Unit:{" "}
+              {saving.calculateUnit !== 0
+                ? saving.calculateUnit
+                : "End of Month"}
             </Typography>
-            <Typography>
-              Frequency: {saving.period} - Unit: {saving.calculateUnit !== 0 ? saving.calculateUnit : "End of Month"}
-            </Typography>
-            <div className='flex justify-between'>
-              <span style={{color: '#37517E'}} variant="small">
+            <div className="flex justify-between">
+              <span style={{ color: "#37517E" }} variant="small">
                 $199 / ${saving.savingGoal}
               </span>
-              <span style={{color: '#37517E'}} variant="small">
+              <span style={{ color: "#37517E" }} variant="small">
                 30%
               </span>
             </div>
-            <Progress color={'#008EC0'} value={67} />
+            <Progress color={"#008EC0"} value={67} />
           </CardBody>
           <CardFooter divider className="flex items-center justify-end py-3">
-            <Typography variant="small">{moment(new Date(saving.deadline)).format('ddd, DD MMM, yyyy')}</Typography>
+            <Typography variant="small">
+              {moment(new Date(saving.deadline)).format("ddd, DD MMM, yyyy")}
+            </Typography>
           </CardFooter>
         </Card>
       </div>
     </>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,5 @@ module.exports = withMT({
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [],
 });
