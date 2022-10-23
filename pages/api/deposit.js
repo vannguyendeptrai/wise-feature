d305e70d6ca2b3ce6eb349599df318898b4778b1
vannguyendeptrai <vannguyen2426@gmail.com>
2022-10-23 08:08:13 +0200

    Delete deposit.js

--- pages/api/deposit.js
+++
@@ -1,6 +0,0 @@
-import prisma from "lib/prisma";
-import { getDepositsOnSaving, getDepositsOnUser } from "lib/data";
-
-const depositsOnSaving = await getDepositsOnSaving(savingId)
-
-const depositOnUser = await getDepositsOnUser(userId)
